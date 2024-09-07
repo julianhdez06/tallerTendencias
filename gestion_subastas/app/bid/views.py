499d5896213f851ad6f7631c03cf87167beaa0bf
Juangonzalez05 <jmgr500@gmail.com>
2024-09-07 11:43:05 -0500

    create serializers bid,views

--- gestion_subastas/app/bid/views.py
+++ gestion_subastas/app/bid/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import renderr
+from .models import Bid
+from .serializers import BidSerializer
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 # Create your views here.
+
+class BidViewSet(viewsets.ModelViewSet):
+    serializer_class = BidSerializer
+    queryset = Bid.objects.all()
+
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+    ]
+    filterset_fields = ('__all__')
+    search_fields = ('__all__')
