7b696ac8db27c0d5d288a5477b16a6638b27ccb6
JDLB0 <juandiegolopezmedellin123@gmail.com>
2024-09-07 11:54:57 -0500

    app auction views

--- gestion_subastas/app/auction/views.py
+++ gestion_subastas/app/auction/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+from .models import Auction
+from rest_framework.generics import ListAPIView,CreateAPIView,RetrieveAPIView,DestroyAPIView,UpdateAPIView
+from .serializer import AuctionSerializer
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 # Create your views here.
+
+class AuctionViewSet(viewsets.ModelViewSet):
+    serializer_class = AuctionSerializer
+    queryset = Auction.objects.all()
+
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+    ]
+    filterset_fields = ('__all__')
+    search_fields = ('__all__')
