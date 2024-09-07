b0bea34f88fa4886d836f45b849855f751bbe654
Juangonzalez05 <jmgr500@gmail.com>
2024-09-07 11:37:21 -0500

    create bid

--- gestion_subastas/app/bid/models.py
+++ gestion_subastas/app/bid/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from app.auction.models import Auction
 
+class Bid(models.Model):
+    subasta = models.ForeignKey(Auction, on_delete=models.CASCADE, related_name='bids')
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    cantidad = models.DecimalField(max_digits=10, decimal_places=2)
 
+    
+
+    def __str__(self):
+        return f"Bid by {self.usuario.username} for {self.subasta.nombre} - ${self.cantidad}"
